760daf2f247acfcb48b73a8c632c8a1bc77ecb03
chunxiao01 <chunxiao01@outlook.com>
2022-03-08 14:27:56 +0800

    [±] 修改【首页-热门】初始化请求数据方法

--- src/views/home/Hot.vue
+++ src/views/home/Hot.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <div class="videolist_container">
     <video-card
-      v-for="(video, index) in videolist"
+      v-for="(video, index) in videolistdata.videolist"
       :key="index"
       :videoinfos="video"
     />
@@ -15,19 +15,37 @@ import { getHomeHotMultidata } from "network/home/hot_request"
 export default {
   data() {
     return {
-      videolist: []
+      videolistdata: {
+        ps: 30,
+        pn: 0,
+        videolist: []
+      }
     }
   },
   components: { VideoCard },
-  mounted() {
-    getHomeHotMultidata().then((res) => {
-      console.log(res.data.data.list)
-      if (res.data.data.list) {
-        this.videolist = res.data.data.list
-      }
-    })
+  created() {
+    this.getHomeHotVideoListData(
+      this.videolistdata.ps,
+      this.videolistdata.pn + 1
+    )
+    console.log(this.videolistdata)
+  },
+  methods: {
+    getHomeHotVideoListData(ps, pn) {
+      getHomeHotMultidata(ps, pn).then((res) => {
+        if (res.data.data.list) {
+          this.videolistdata.videolist.push(...res.data.data.list)
+          this.videolistdata.pn += 1
+        }
+      })
+    }
   }
 }
 </script>
 
-<style></style>
+<style>
+.videolist_container {
+  height: 630px;
+  overflow: auto;
+}
+</style>
